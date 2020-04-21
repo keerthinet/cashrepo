d21a1bbe281e1e93b2e12dbaf960a1e0ce0cc4ca
keerthinet <63235418+keerthinet@users.noreply.github.com>
2020-04-21 17:28:40 +0530

    Update dbo.Table_1.sql

--- dbo.Table_1.sql
+++ dbo.Table_1.sql
@@ -1,6 +1,6 @@
 CREATE TABLE [dbo].[Table_1]
 (
 [CustName] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-[acc] [numeric] (18, 0) NULL
+[acc 121] [numeric] (18, 0) NULL
 ) ON [PRIMARY]
 GO
