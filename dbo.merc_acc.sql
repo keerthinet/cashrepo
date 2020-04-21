c60b0b98f60a8e2582b4ba9f1c2f7a22e8d8d51d
keerthinet <63235418+keerthinet@users.noreply.github.com>
2020-04-21 17:46:10 +0530

    Update dbo.merc_acc.sql
    col name comment

--- dbo.merc_acc.sql
+++ dbo.merc_acc.sql
@@ -5,6 +5,7 @@ CREATE TABLE [dbo].[merc_acc]
 [Name] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 [city] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 [testcol] [nvarchar] (max) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-[comment] [numeric] (18, 0) NULL
+[comment] [numeric] (18, 0) NULL,
+[comment100] [numeric] (18, 0) NULL
 ) ON [PRIMARY]
 GO
